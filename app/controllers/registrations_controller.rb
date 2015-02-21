17c059fe9d8c2220198c32c2ec4ee40b7b7b1192
alessandrostein <alessandro@alessandrostein.com>
2015-02-20 22:51:56 -0200

    Remove debugger

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,7 +10,6 @@ class RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-    byebug
     build_resource(sign_up_params)
     if resource.save
       yield resource if block_given?
